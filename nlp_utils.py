ab3e41afefc855b8149ae59ee35514941729ce56
gal_tchinio <tchgal@gmail.com>
2024-07-13 21:36:54 +0300

    Minor changes

--- nlp_utils.py
+++ nlp_utils.py
@@ -1,33 +1,52 @@
-
-
 from collections import defaultdict
 import os
 import pandas as pd
+import getpass
 
 
 def get_animals_data_df(data_path=None, limit_per_class=10):
-    data_path = data_path or '/home/dcor/roeyron/interpretability_multi_hop/patchscopes/code/nlp_project/animals10/raw-img/',
-    translate = {'cane': 'dog', 'cavallo': 'horse', 'elefante': 'elephant','farfalla': 'butterfly','gallina': 'hen','gatto': 'cat','mucca': 'cow', 'pecora': 'sheep','ragno': 'spider','scoiattolo': 'squirrel'}
+    data_path = (
+        data_path or "/home/dcor/roeyron/interpretability_multi_hop/patchscopes/code/nlp_project/animals10/raw-img/"
+    )
+    translate = {
+        "cane": "dog",
+        "cavallo": "horse",
+        "elefante": "elephant",
+        "farfalla": "butterfly",
+        "gallina": "hen",
+        "gatto": "cat",
+        "mucca": "cow",
+        "pecora": "sheep",
+        "ragno": "spider",
+        "scoiattolo": "squirrel",
+    }
     ddl = defaultdict(list)
     for class_dir_name in sorted(os.listdir(data_path)):
         class_dir_path = os.path.join(data_path, class_dir_name)
         img_names = sorted(os.listdir(class_dir_path))[:limit_per_class]
         for img_name in img_names:
             img_path = os.path.join(class_dir_path, img_name)
-            ddl['class_name'].append(translate[class_dir_name])
-            ddl['image_path'].append(img_path)
+            ddl["class_name"].append(translate[class_dir_name])
+            ddl["image_path"].append(img_path)
     return pd.DataFrame(ddl)
 
 
 def get_celeba_data_df(data_path=None, limit=200):
-    dataset_path = data_path or '/home/dcor/roeyron/interpretability_multi_hop/patchscopes/code/celeba-dataset'
-    eval_path = os.path.join(dataset_path, 'list_attr_celeba.csv')
+    dataset_path = data_path or "/home/dcor/roeyron/interpretability_multi_hop/patchscopes/code/celeba-dataset"
+    eval_path = os.path.join(dataset_path, "list_attr_celeba.csv")
     df = pd.read_csv(eval_path)
     if limit:
         df = df.sample(limit, random_state=42)
-    df['image_path'] = df.image_id.apply(lambda image_id: os.path.join(dataset_path, 'img_align_celeba/img_align_celeba/', image_id))
+    df["image_path"] = df.image_id.apply(
+        lambda image_id: os.path.join(dataset_path, "img_align_celeba/img_align_celeba/", image_id)
+    )
     df = df[list(df.columns[-1:]) + list(df.columns[:-1])]
     df = df.applymap(lambda x: {-1: False, 1: True}.get(x, x))
     return df
 
 
+def get_cache_dir() -> str:
+    user = getpass.getuser()
+    if user == "roeyron":
+        return "/home/dcor/roeyron/.cache/"
+    return f"/home/joberant/NLP_2324/{user}/.cache/"
--- text_conditioned_image_embedding.py
+++ text_conditioned_image_embedding.py
@@ -1,4 +1,3 @@
-
 from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
 import torch
 from PIL import Image
@@ -16,13 +15,13 @@ DATAFRAME_CHUNK_SIZE = 400
 
 
 QUESTIONS = [
-    'describe the hair of the person in the image',
-    'describe the expression of the person in the image',
-    'describe the background color of the image',
-    'what is the style of the image?',
-    'look on the lips of the person in the image',
-    'describe the shirt of the person in the image',
-    'describe the nose of the person in the image',
+    "describe the hair of the person in the image",
+    "describe the expression of the person in the image",
+    "describe the background color of the image",
+    "what is the style of the image?",
+    "look on the lips of the person in the image",
+    "describe the shirt of the person in the image",
+    "describe the nose of the person in the image",
 ]
 
 
@@ -43,11 +42,13 @@ def get_list_subset(lst, n_parts, part_ind):
 
 
 def main(n_parts, part, output_dir_path):
-    cache_dir = '/home/dcor/roeyron/.cache/'
-    device = 'cuda'
+    cache_dir = "/home/dcor/roeyron/.cache/"
+    device = "cuda"
     model_name = "llava-hf/llava-v1.6-mistral-7b-hf"
     processor = LlavaNextProcessor.from_pretrained(model_name, cache_dir=cache_dir)
-    model = LlavaNextForConditionalGeneration.from_pretrained(model_name, torch_dtype=torch.float16, cache_dir=cache_dir)
+    model = LlavaNextForConditionalGeneration.from_pretrained(
+        model_name, torch_dtype=torch.float16, cache_dir=cache_dir
+    )
     model.generation_config.pad_token_id = processor.tokenizer.pad_token_id
     model = model.to(device)
     df_data = nlp_utils.get_celeba_data_df(limit=1200)
@@ -55,7 +56,7 @@ def main(n_parts, part, output_dir_path):
     ddl_inference = defaultdict(list)
 
     question_indices = get_list_subset(list(range(len(QUESTIONS))), n_parts, part)
-    pbar = tqdm(total=len(question_indices) * len(df_data), desc='Extract hidden states')
+    pbar = tqdm(total=len(question_indices) * len(df_data), desc="Extract hidden states")
     for q_ind in question_indices:
         question = QUESTIONS[q_ind]
 
@@ -64,31 +65,33 @@ def main(n_parts, part, output_dir_path):
             image = Image.open(dp_row.image_path)
             prompt = f"[INST] <image>\n{question} [/INST]"
             inputs = processor(prompt, image, return_tensors="pt").to(device)
-            output = model.generate(**inputs, max_new_tokens=100, output_hidden_states=True, return_dict_in_generate=True)
-            hidden_states = output['hidden_states']
+            output = model.generate(
+                **inputs, max_new_tokens=100, output_hidden_states=True, return_dict_in_generate=True
+            )
+            hidden_states = output["hidden_states"]
 
-            input_ids = list(inputs['input_ids'][0].detach().cpu().numpy())      # [1, 733, 16289
-            output_ids = list(output['sequences'][0].detach().cpu().numpy())     # [1, 733, 16289, ...
-            ddl_inference['question'].append(question)
-            ddl_inference['image_id'].append(dp_row.image_id)
-            ddl_inference['image_path'].append(dp_row.image_path)
-            ddl_inference['input_ids'].append(input_ids)
-            ddl_inference['output_ids'].append(output_ids)
+            input_ids = list(inputs["input_ids"][0].detach().cpu().numpy())  # [1, 733, 16289
+            output_ids = list(output["sequences"][0].detach().cpu().numpy())  # [1, 733, 16289, ...
+            ddl_inference["question"].append(question)
+            ddl_inference["image_id"].append(dp_row.image_id)
+            ddl_inference["image_path"].append(dp_row.image_path)
+            ddl_inference["input_ids"].append(input_ids)
+            ddl_inference["output_ids"].append(output_ids)
             hidden_states = get_reduced_hidden_states_to_store(hidden_states, len(input_ids))
-            ddl_inference['hidden_states'].append(hidden_states)
+            ddl_inference["hidden_states"].append(hidden_states)
             pbar.update()
 
             if dp_ind % DATAFRAME_CHUNK_SIZE == 0:
                 df_inference = pd.DataFrame(ddl_inference)
-                out_file_path = os.path.join(output_dir_path, f'qind_{q_ind:02d}_chunk_{chunk_ind:02d}.pickle')
-                with open(out_file_path, 'wb') as f:
+                out_file_path = os.path.join(output_dir_path, f"qind_{q_ind:02d}_chunk_{chunk_ind:02d}.pickle")
+                with open(out_file_path, "wb") as f:
                     pickle.dump(df_inference, f)
                 ddl_inference = defaultdict(list)
                 chunk_ind += 1
 
 
 def run():
-    output_dir_path = '/home/dcor/roeyron/interpretability_multi_hop/patchscopes/code/nlp_project/inference_cond_embd_1'
+    output_dir_path = "/home/dcor/roeyron/interpretability_multi_hop/patchscopes/code/nlp_project/inference_cond_embd_1"
     if not os.path.exists(output_dir_path):
         os.mkdir(output_dir_path)
     args = sys.argv
@@ -99,8 +102,8 @@ def run():
     except:
         n_parts = 1
         part = 0
-    print('n_parts', n_parts)
-    print('part', part)
+    print("n_parts", n_parts)
+    print("part", part)
     main(n_parts, part, output_dir_path)
 
 
